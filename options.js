fe737cc960eb45eaec872fa5a2dc26571b15d707
softacoder <113185323+softacoder@users.noreply.github.com>
2023-10-04 12:41:44 +0100

    commit

--- options.js
+++ options.js
@@ -2,5 +2,13 @@ const nameInput = document.getElementById("name-input");
 const saveBtn = document.getElementById("save-btn");
 
 saveBtn.addEventListener("click", () => {
-  console.log(nameInput.value);
+  const name = nameInput.value;
+  chrome.storge.sync.set(
+    {
+      name,
+    },
+    () => {
+      console.log("Name is set ${name}");
+    }
+  );
 });
