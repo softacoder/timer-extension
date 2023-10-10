02bfece1b5b088b1f08305d5a04df47e2d6267cb
softacoder <113185323+softacoder@users.noreply.github.com>
2023-10-10 14:54:38 +0100

    commit

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ const timerElement = document.getElementById("timer");
 function updateTimeElements() {
   chrome.storage.local.get(["timer"], (res) => {
     const time = res.timer ?? 0;
-    timerElement.textContent = `The timer is at ${time} seconds`;
+    timerElement.textContent = `The timer is at: ${time} seconds`;
   });
   const currentTime = new Date().toLocaleTimeString();
   timeElement.textContent = `The time is: ${currentTime}`;
@@ -19,4 +19,25 @@ chrome.storage.sync.get(["name"], (res) => {
   nameElement.textContent = `Your name is: ${name}`;
 });
 
-console.log(this);
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+
+startBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: true,
+  });
+});
+
+stopBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: false,
+  });
+});
+
+resetBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    timer: 0,
+    isRunning: false,
+  });
+});
